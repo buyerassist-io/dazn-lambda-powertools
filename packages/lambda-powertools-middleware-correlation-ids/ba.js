2bf24884dda68973575f41581f13a8ca2c82b71c
Amit Dugar <amit@buyerassist.io>
2022-06-22 17:36:44 +0530

    zapier related logs will have zap- in span/traceids

--- packages/lambda-powertools-middleware-correlation-ids/ba.js
+++ packages/lambda-powertools-middleware-correlation-ids/ba.js
@@ -2,9 +2,13 @@ const consts = require("./consts");
 const uuid = require("uuid");
 const updateBARelatedCorrelationIds = (correlationIds) => {
   // BuyerAssist Logic to generate SpanIds/TraceIds
-  const spanId = generateSpanId();
+  let externalSource = ""
+  if (correlationIds[consts.USER_AGENT] && correlationIds[consts.USER_AGENT].match(/zap/i)) {
+    externalSource = "zap"
+  }
+  const spanId = generateSpanId(externalSource);
   if (!correlationIds[consts.X_CORRELATION_TRACE_ID]) {
-    correlationIds[consts.X_CORRELATION_TRACE_ID] = generateTraceId();
+    correlationIds[consts.X_CORRELATION_TRACE_ID] = generateTraceId(externalSource);
   }
   if (correlationIds[consts.X_CORRELATION_SPAN_ID]) {
     correlationIds[consts.X_CORRELATION_PARENT_ID] =
@@ -12,11 +16,11 @@ const updateBARelatedCorrelationIds = (correlationIds) => {
   }
   correlationIds[consts.X_CORRELATION_SPAN_ID] = spanId;
 };
-const generateTraceId = () => {
-  return uuid.v1();
+const generateTraceId = (externalSource) => {
+  return `${externalSource}-${uuid.v1()}`;
 };
-const generateSpanId = () => {
-  return `${uuid.v1()}-${process.env.SRV_DISPLAY_NAME}`;
+const generateSpanId = (externalSource) => {
+  return `${externalSource}-${uuid.v1()}-${process.env.SRV_DISPLAY_NAME}`;
 };
 module.exports = {
   updateBARelatedCorrelationIds,
